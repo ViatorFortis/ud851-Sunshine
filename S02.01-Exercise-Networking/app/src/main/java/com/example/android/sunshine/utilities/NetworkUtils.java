22ab603a87eb01e1eab91eede1b2d5d4fea9e123
Антон <strider-13@mail.ru>
2017-12-10 19:59:21 +1000

    Sunshine 2 Exercise 1 TODO 1 buildUrl method fixed

--- S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/utilities/NetworkUtils.java
+++ S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/utilities/NetworkUtils.java
@@ -20,6 +20,7 @@ import android.net.Uri;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
@@ -75,7 +76,14 @@ public final class NetworkUtils {
                 .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
                 .build();
 
-        return null;
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return url;
     }
 
     /**
